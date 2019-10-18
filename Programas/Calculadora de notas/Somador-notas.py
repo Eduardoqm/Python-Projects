ae78cf649463e46d1d4f525119f689677401e7d8
Eduardoqm <eduardobio2009@gmail.com>
2019-10-17 22:23:54 -0300

    Update Somador-notas.py

--- Programas/Calculadora de notas/Somador-notas.py
+++ Programas/Calculadora de notas/Somador-notas.py
@@ -16,14 +16,20 @@ def soma():
     b = c
 
 master = Tk()
-master.title('Percent Calc - Beta')
-master.geometry('300x100+0+0') #L,A
+master.title('Calculadora de notas - Beta')
+master.geometry('700x400+0+0') #L,A
 
 Label(master, text='= N1').grid(row=0, column=3) 
 result = Label(master, text='').grid(row=3, column=2) 
 
-e1 = Entry(master) 
-e1.grid(row=0, column=1) 
+cat_nt = Entry(master) 
+cat_nt.grid(row=0, column=1)
+n_nt = Entry(master) 
+n_nt.grid(row=0, column=2)
+data_nt = Entry(master)
+data_nt.grid(row=0, column=3)
+val_nt = Entry(master)  
+val_nt.grid(row=0, column=4) 
 
 somabt = Button(master, text='Somar', command=soma)
 somabt.grid(row=3, column=0)
