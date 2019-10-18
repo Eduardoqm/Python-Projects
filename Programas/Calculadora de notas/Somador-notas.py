adeaa2bf53e7db3cfc6f98733875e4648c6b90f3
Eduardoqm <eduardobio2009@gmail.com>
2019-10-17 22:30:40 -0300

    Update Somador-notas.py

--- Programas/Calculadora de notas/Somador-notas.py
+++ Programas/Calculadora de notas/Somador-notas.py
@@ -7,7 +7,7 @@ Created on Sun Aug  4 18:28:43 2019
 
 from tkinter import *
 
-def soma():
+def add():
     a=float(e1.get())
     b=17.07
     c = a + b
@@ -19,23 +19,34 @@ master = Tk()
 master.title('Calculadora de notas - Beta')
 master.geometry('700x400+0+0') #L,A
 
-Label(master, text='= N1').grid(row=0, column=3) 
-result = Label(master, text='').grid(row=3, column=2) 
+#Labels (texts)
+Label(master, text='Category').grid(row=0, column=1) 
+Label(master, text='Number ID').grid(row=0, column=2) 
+Label(master, text='Date').grid(row=0, column=3) 
+Label(master, text='Value').grid(row=0, column=4) 
+result = Label(master, text='').grid(row=3, column=5) 
 
+#Inputs
 cat_nt = Entry(master) 
-cat_nt.grid(row=0, column=1)
+cat_nt.grid(row=1, column=1)
 n_nt = Entry(master) 
-n_nt.grid(row=0, column=2)
+n_nt.grid(row=1, column=2)
 data_nt = Entry(master)
-data_nt.grid(row=0, column=3)
+data_nt.grid(row=1, column=3)
 val_nt = Entry(master)  
-val_nt.grid(row=0, column=4) 
+val_nt.grid(row=1, column=4) 
 
-somabt = Button(master, text='Somar', command=soma)
-somabt.grid(row=3, column=0)
+#Buttons
+addbt = Button(master, text='Add', command=add)
+addbt.grid(row=1, column=6)
 
+addbt = Button(master, text='Add', command=add)
+addbt.grid(row=1, column=6)
 
+addbt = Button(master, text='Add', command=add)
+addbt.grid(row=1, column=6)
 
-
+addbt = Button(master, text='Add', command=add)
+addbt.grid(row=1, column=6)
 
 master.mainloop()
