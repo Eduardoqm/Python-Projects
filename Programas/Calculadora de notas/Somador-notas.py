754abbb41af21f5b4a3096787d9e059e7a5b20e9
Eduardoqm <eduardobio2009@gmail.com>
2019-10-22 14:48:20 -0300

    Update Somador-notas.py

--- Programas/Calculadora de notas/Somador-notas.py
+++ Programas/Calculadora de notas/Somador-notas.py
@@ -23,19 +23,25 @@ def opn():
     resmax = df['values'].max()
     resmin = df['values'].min()
 
-    Label(master, text = 'On average you are spending per purchase:', fg = "black", bg = "white",
+    Label(master, text = 'On average you are spending per purchase:', fg = "black", bg = "snow",
     font = "Verdana 15", justify = LEFT).place(relx=.3, rely=.35, anchor=S)
-    Label(master, text = resmean, fg = "black", bg = "white",
+    Label(master, text = '      ', fg = "black", bg = "snow",
+    font = "Verdana 15 bold", justify = LEFT).place(relx=.3, rely=.4, anchor=S)
+    Label(master, text = resmean, fg = "black", bg = "snow",
     font = "Verdana 15 bold", justify = LEFT).place(relx=.3, rely=.4, anchor=S)
 
-    Label(master, text = 'The maximum amount spent so far is', fg = "black", bg = "white",
+    Label(master, text = 'The maximum amount spent so far is', fg = "black", bg = "snow",
     font = "Verdana 15", justify = LEFT).place(relx=.3, rely=.45, anchor=S)
-    Label(master, text = resmax, fg = "black", bg = "white",
+    Label(master, text = '      ', fg = "black", bg = "snow",
+    font = "Verdana 15 bold", justify = LEFT).place(relx=.5, rely=.45, anchor=S)
+    Label(master, text = resmax, fg = "black", bg = "snow",
     font = "Verdana 15 bold", justify = LEFT).place(relx=.5, rely=.45, anchor=S)
 
-    Label(master, text = 'The minimum amount spent so far is', fg = "black", bg = "white",
+    Label(master, text = 'The minimum amount spent so far is', fg = "black", bg = "snow",
     font = "Verdana 15", justify = LEFT).place(relx=.3, rely=.5, anchor=S)
-    Label(master, text = resmin, fg = "black", bg = "white",
+    Label(master, text = '      ', fg = "black", bg = "snow",
+    font = "Verdana 15 bold", justify = LEFT).place(relx=.5, rely=.5, anchor=S)
+    Label(master, text = resmin, fg = "black", bg = "snow",
     font = "Verdana 15 bold", justify = LEFT).place(relx=.5, rely=.5, anchor=S)
 
     
@@ -80,6 +86,31 @@ def add():
     Label(master, text = res, fg = "black", bg = "white",
     font = "Verdana 10", justify = CENTER).place(relx=.3, rely=.9, anchor=S)
 
+    resmean = df['values'].mean()
+    resmax = df['values'].max()
+    resmin = df['values'].min()
+
+    Label(master, text = 'On average you are spending per purchase:', fg = "black", bg = "snow",
+    font = "Verdana 15", justify = LEFT).place(relx=.3, rely=.35, anchor=S)
+    Label(master, text = '      ', fg = "black", bg = "snow",
+    font = "Verdana 15 bold", justify = LEFT).place(relx=.3, rely=.4, anchor=S)
+    Label(master, text = resmean, fg = "black", bg = "snow",
+    font = "Verdana 15 bold", justify = LEFT).place(relx=.3, rely=.4, anchor=S)
+
+    Label(master, text = 'The maximum amount spent so far is', fg = "black", bg = "snow",
+    font = "Verdana 15", justify = LEFT).place(relx=.3, rely=.45, anchor=S)
+    Label(master, text = '      ', fg = "black", bg = "snow",
+    font = "Verdana 15 bold", justify = LEFT).place(relx=.5, rely=.45, anchor=S)
+    Label(master, text = resmax, fg = "black", bg = "snow",
+    font = "Verdana 15 bold", justify = LEFT).place(relx=.5, rely=.45, anchor=S)
+
+    Label(master, text = 'The minimum amount spent so far is', fg = "black", bg = "snow",
+    font = "Verdana 15", justify = LEFT).place(relx=.3, rely=.5, anchor=S)
+    Label(master, text = '      ', fg = "black", bg = "snow",
+    font = "Verdana 15 bold", justify = LEFT).place(relx=.5, rely=.5, anchor=S)
+    Label(master, text = resmin, fg = "black", bg = "snow",
+    font = "Verdana 15 bold", justify = LEFT).place(relx=.5, rely=.5, anchor=S)
+
 def plott():
     df.plot()
     plt.ylabel('Value')
@@ -98,7 +129,7 @@ Label(master, text='                     Data Bank                    ',
 fg = "black", bg = "lavender", font = "Verdana 15 bold").place(relx=.8, rely=.95, anchor=CENTER)
 
 Label(master, text = 'Summarized report', fg = "black", bg = "lavender",
-font = "Verdana 20 bold", justify = LEFT).place(relx=.3, rely=.25, anchor=S)
+font = "Verdana 25 bold", justify = LEFT).place(relx=.3, rely=.25, anchor=S)
 
 Label(master, text='Category', bg = "white", font = "Verdana 10 bold").grid(row=0, column=2) 
 Label(master, text='Number ID', bg = "white", font = "Verdana 10 bold").grid(row=0, column=3) 
