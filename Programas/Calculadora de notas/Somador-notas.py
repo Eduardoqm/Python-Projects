b0b6176dffee9dd5378b10d5527bd608758573ec
Eduardoqm <eduardobio2009@gmail.com>
2019-10-24 12:28:23 -0300

    Update Somador-notas.py

--- Programas/Calculadora de notas/Somador-notas.py
+++ Programas/Calculadora de notas/Somador-notas.py
@@ -135,22 +135,23 @@ def plott():
    
 
     #Pieplot
-    #plt.figure(figsize=(13, 4))
-    #plt.subplot(1, 2, 1)
-    #df.groupby('category')['values'].sum().plot(kind='pie')
-    #plt.ylabel(' ')
-    #plt.xlabel(' ')
-    #plt.title('Size of Category')
+    plt.figure(figsize=(13, 4))
+    plt.subplot(2, 2, 1)
+    df.groupby('category')['values'].sum().plot(kind='pie')
+    plt.ylabel(' ')
+    plt.xlabel(' ')
+    plt.title('Size of Category')
    
     #Barplot
-    #plt.subplot(1, 2, 2)
-    #df.groupby('category')['values'].sum().plot(kind='bar', color = ['green','y','orange','yellow', 'red', 'darkgreen', 'blue'])
-    #plt.ylabel('Total in money')
-    #plt.xlabel(' ')
-    #plt.title('Values by category')
-    #plt.xticks(rotation='45')
+    plt.subplot(2, 2, 2)
+    df.groupby('category')['values'].sum().plot(kind='bar', color = ['green','y','orange','yellow', 'red', 'darkgreen', 'blue'])
+    plt.ylabel('Total in money')
+    plt.xlabel(' ')
+    plt.title('Values by category')
+    plt.xticks(rotation='45')
 
     #Lineplot
+    plt.subplot(2, 2, 3)
     plt.plot(df.groupby('date')['values'].sum(), color = 'orange')
     mdates.DateFormatter("%Y-%m-%d")
     plt.title('Spending over time')
