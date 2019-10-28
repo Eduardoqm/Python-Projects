5770810087cbb5cc3f48174c52a9e0abb31306f1
Eduardoqm <eduardobio2009@gmail.com>
2019-10-28 18:06:21 -0300

    CSV data

--- Programas/Computer Port Reading/Serial Reading and Save csv.py
+++ Programas/Computer Port Reading/Serial Reading and Save csv.py
@@ -1,4 +1,6 @@
 import serial
+import time
+import csv
 
 port = "COM4"
 ser = serial.Serial(port, 9600, timeout=5.0)
@@ -6,4 +8,6 @@ while True:
     ser.flushInput()
     response = ser.readline()
     print(response)
-    print(' ')
+    with open("Test_COM4.csv","a") as f:
+        writer = csv.writer(f,delimiter=",")
+        writer.writerow([time.time(),response])
