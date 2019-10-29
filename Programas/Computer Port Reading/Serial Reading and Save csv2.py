5b231463b4896d3dfaa8f49fe8caab1b4ceb6df8
Eduardoqm <eduardobio2009@gmail.com>
2019-10-28 23:25:47 -0300

    Update Serial Reading and Save csv2.py

--- Programas/Computer Port Reading/Serial Reading and Save csv2.py
+++ Programas/Computer Port Reading/Serial Reading and Save csv2.py
@@ -15,8 +15,11 @@ while True:
         decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
         print(decoded_bytes)
 
+        t = time.localtime()
+        current_time = time.strftime("%H:%M:%S", t)
+
         #df_input = pd.DataFrame({'Time':[float(time.time())], 'Moisture':[float(decoded_bytes)]})
-        df_input = pd.DataFrame({'Time':[float(time.time())], 'Moisture':[float(decoded_bytes)]})
+        df_input = pd.DataFrame({'Time':[float(current_time)], 'Moisture':[float(decoded_bytes)]})
         df_add = [df, df_input]
         df_master = pd.concat(df_add, ignore_index=True)
         df = df_master
