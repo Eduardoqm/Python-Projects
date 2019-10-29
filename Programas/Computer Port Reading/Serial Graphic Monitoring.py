f0b6ee72f229e18af6ebe19b5c3556bbd74249cd
Eduardoqm <eduardobio2009@gmail.com>
2019-10-29 11:51:31 -0300

    Data report

---
+++ Programas/Computer Port Reading/Serial Graphic Monitoring.py
@@ -0,0 +1,17 @@
+import time
+import pandas as pd
+from datetime import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.dates as mdates
+
+data = pd.read_csv("Test_COM4.csv")
+        
+data.plot(kind='line',x='Time',y='Moisture',color='red')
+plt.title('Soil Moisture')
+plt.ylabel('Moisture (%)')
+plt.xticks(rotation = '90')
+#plt.show(block=False)
+#plt.ion()
+plt.show()
+        
