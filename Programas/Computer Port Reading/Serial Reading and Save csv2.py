dd5b9dc1be82eca778479eae5c418cc5900cda91
Eduardoqm <eduardobio2009@gmail.com>
2019-10-28 23:24:10 -0300

    Time

--- Programas/Computer Port Reading/Serial Reading and Save csv2.py
+++ Programas/Computer Port Reading/Serial Reading and Save csv2.py
@@ -1,6 +1,5 @@
 import serial
 import time
-import csv
 import pandas as pd
 
 global df
@@ -15,10 +14,8 @@ while True:
         ser_bytes = ser.readline()
         decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
         print(decoded_bytes)
-        #with open("test_data.csv","a") as f:
-            #writer = csv.writer(f,delimiter=",")
-            #writer.writerow([time.time(),decoded_bytes])
 
+        #df_input = pd.DataFrame({'Time':[float(time.time())], 'Moisture':[float(decoded_bytes)]})
         df_input = pd.DataFrame({'Time':[float(time.time())], 'Moisture':[float(decoded_bytes)]})
         df_add = [df, df_input]
         df_master = pd.concat(df_add, ignore_index=True)
