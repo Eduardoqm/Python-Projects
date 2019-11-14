d484057089c50e867fc0d8a8bb4228087b3cc54c
Eduardoqm <eduardobio2009@gmail.com>
2019-11-14 11:15:24 -0300

    Create Soil Moisture Interface.py

---
+++ Programas/Computer Port Reading/Soil Moisture Interface.py
@@ -0,0 +1,19 @@
+import serial
+import time
+import pandas as pd
+from datetime import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.dates as mdates
+from tkinter import *
+
+master = Tk()
+master.title('Percent Calc - Beta')
+master.geometry('500x200+0+0') #L,A
+
+
+
+
+
+
+master.mainloop()
