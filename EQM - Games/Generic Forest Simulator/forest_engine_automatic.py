69152f873e5cc2a5364945a6eb304364ca8563ca
Eduardoqm <eduardobio2009@gmail.com>
2020-01-10 08:26:16 -0300

    Update forest_engine_automatic.py

--- EQM - Games/Generic Forest Simulator/forest_engine_automatic.py
+++ EQM - Games/Generic Forest Simulator/forest_engine_automatic.py
@@ -9,6 +9,7 @@ import time
 tree = int(100)
 lit = int(25000)
 nut = int(0)
+turn = int(0)
 
 #Everionment variables
 mostair = int(100)
@@ -17,27 +18,38 @@ temp = float(25)
 
 #Logic engine
 while True:
-    time.sleep(1)
+    time.sleep(0.1)
     os.system('cls')
     print('-------------------------------CONTROL PANEL------------------------------------')
     print('''
     Trees: {} | Litter: {} | Nutrients per tree: {}
+
     Temperature: {}ºC | Air Moisture: {} | Soil Moisture: {}'''.format(tree, lit, nut, temp, mostair, mosts))
     print(' ')
+    print('Age: {}'.format(turn))
     
     print('--------------------------------------------------------------------------------')
     print(' ')
-    print('-----------------------------Ecossistem Command---------------------------------')
+    print('---------------------------Ecossistem Autocommand-------------------------------')
     #tempv = float(input('Temp Regulation (input +5 to -5): '))
     #mostairv = int(input('Air Moisture variation (input +10 to -10): '))
     
     if tree <= 0.0:
-        tree = int(0)
-        print('The forest die...')
-        time.sleep(2)
-        print('GAME OVER!!!')
-        time.sleep(5)
-        
+        tree = 3
+        temp = 40
+        lit = int(0)
+        mosts = int(0)
+        mostair = int(0)
+        #print('The forest die...')
+        #time.sleep(2)
+        #print('GAME OVER!!!')
+        #time.sleep(5)
+   
+    elif tree >= 350:
+        tree = 300
+        lit = (tree*10)*(temp/2)
+        nut = (((lit/tree)/100)/100)*mosts
+
     elif temp > 40 and mostair < 40:
         os.system('cls')
         print('ALERT FIRE CONDITIONS ARE DETECTED!!!')
@@ -51,7 +63,10 @@ while True:
         time.sleep(3)
         #mortef = (temp*lit)/100
         tree = tree/2.5
-        temp = 35
+        temp = 30
+        lit = int(0)
+        mosts = int(0)
+        mostair = int(20)
 
     elif temp > 40 and mosts < 50:
         os.system('cls')
@@ -66,9 +81,12 @@ while True:
         time.sleep(3)
         #mortef = (temp*lit)/100
         tree = tree/1.7
-        temp = 35
+        temp = 30
+        lit = int(0)
+        mosts = int(0)
+        mostair = int(20)
 
-    elif temp >= 60:
+    elif temp >= 55:
         os.system('cls')
         print('ALERT FIRE CONDITIONS ARE DETECTED!!!')
         time.sleep(1)
@@ -81,21 +99,34 @@ while True:
         time.sleep(3)
         #mortef = (temp*lit)/100
         tree = tree/2
-        temp = 35
+        temp = 30
+        lit = int(0)
+        mosts = int(0)
+        mostair = int(20)
+
+    elif mostair >= 100:
+        mostair = int(60)
+        mosts = mostair-(tree/50)
+
 
     else:
         #temp = temp+tempv
         #mostair = mostair+mostairv
 
         temp = float((5/tree)+temp)
-        lit = (tree*10)*(temp/2)
-        nut = (lit/tree)/100
+        #temp = float((20/tree)+temp)   
 
         #Recruit and die
         morten = (100-((nut-100)+100))/75
         tree = tree - morten
-        recruit = nut
+        recruit = nut/1.2
         tree = tree+recruit
 
+        #mosts = mostair+(tree/25)
         mostair = tree - 5
-        mosts = mostair+(tree/25)
+        mosts = mostair-(tree/50)
+
+        lit = (tree*10)*(temp/2)
+        nut = (((lit/tree)/100)/100)*mosts
+
+        turn = turn+1
