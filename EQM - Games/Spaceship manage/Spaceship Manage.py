e7868b1b4da65bcc0882efa9b784f4fc629d1104
Eduardoqm <eduardobio2009@gmail.com>
2020-01-10 22:52:04 -0300

    Create Spaceship Manage.py

---
+++ EQM - Games/Spaceship manage/Spaceship Manage.py
@@ -0,0 +1,75 @@
+#Spaceship Manage
+#Eduardo Q Marques 10/01/2020
+
+#Nave Runs
+while True:
+    print('''
+    Ship Locations
+    -----------------------------------
+    1-Mean Control room
+    2-Life Machine room
+    3-Storage room
+    4-Computer room
+    5-Machine room
+    -----------------------------------
+
+    ''')
+
+    local = int(input('Choose a local: '))
+    if local == 1:
+        print('''
+        1-Navegation panel
+        2-Computer panel
+        3-Power panel
+        4-Life conditions panel
+        5-exit
+        ''')
+
+    elif local == 2:
+        print('''
+        Oxigen generator
+        Carbon filters
+        Water recycler
+        Temperature regulations
+        exit
+        ''')
+    
+    elif local == 3:
+        print('Need to put something')
+
+    elif local == 4:
+        print('''
+        1-Power regulators
+        2-Distribution cables
+        3-Hardware computer
+        ''')
+        res = int(input('Choose a piece: ')
+
+        if res == 1:
+            print('''
+            1-Swith off
+            2-Restart
+            3-Change fuse
+            ''')
+
+        elif res == 3:
+            print('''
+            1-Processors
+            2-Memories
+            3-Hard Memory
+            ''')
+
+    elif local == 5:
+        print('''
+        1-Propulsion Engine
+
+        2-Correction Engine
+        Pulse chambers cables
+        Chambers selector
+        Fuel general switch
+
+        3-Power generators
+        General switch
+        Coolers
+        Switch panel
+        ''')    
